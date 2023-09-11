34a3219227dad0b25beb60e3c64de9ee6103e7b0
HimanshuImarticus <himanshu.sharma@imarticus.com>
2023-09-11 17:42:40 +0530

    some changes in .travis file

--- app/src/main/java/com/example/circlecidemo/MainActivity.java
+++ app/src/main/java/com/example/circlecidemo/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("demo","demo");
         Log.d("demoApp","demoApp");
+
     }
 }
